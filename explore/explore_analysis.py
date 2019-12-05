b547a097acf11c813e72a61481c314ef5ecd4b54
bifeng <hubifengmail@163.com>
2019-12-05 14:30:13 +0800

    update preprocess and explore

--- explore/explore_analysis.py
+++ explore/explore_analysis.py
@@ -1,3 +1,4 @@
+from sklearn.decomposition import PCA
 import seaborn as sns
 import matplotlib.pyplot as plt
 import missingno as msno
@@ -22,37 +23,46 @@ def explore_global_plot(data, label='label', n_feats=50, id=None, task='classifi
             print('{} is duplicated !!!'.format(id))
 
         columns.remove(id)
+        data.drop(id, axis=1, inplace=True)
 
     discrete_features = [True if ptypes.is_string_dtype(i) else False for i in data[columns].dtypes]
-    discrete_names = [columns[i] for i,v in enumerate(discrete_features) if v]
-    numeric_names = list(set(columns)-set(discrete_names))
+    discrete_names = [columns[i] for i, v in enumerate(discrete_features) if v]
+    numeric_names = list(set(columns) - set(discrete_names))
 
-    if task=='classification':
-        # data distribution for each class
-        famd = prince.FAMD(
-            n_components=2,
-            n_iter=3,
-            copy=True,
-            check_input=True,
-            engine='auto',
-            random_state=42
-        )
-        famd = famd.fit(data[columns])
-        ax = famd.plot_row_coordinates(
-            data,
-            ax=None,
-            x_component=0,
-            y_component=1,
-            labels=data.index,
-            color_labels=['{}'.format(t) for t in data[label]],
-            ellipse_outline=False,
-            ellipse_fill=True,
-            show_points=True
-        )
-        plt.show()
+    if task == 'classification':
+        if len(discrete_names):
+            # data distribution for each class
+            new_data = data.dropna(axis=0)
+            famd = prince.FAMD(
+                n_components=2,
+                n_iter=3,
+                copy=True,
+                check_input=True,
+                engine='auto',
+                random_state=42
+            )
+            famd = famd.fit(new_data[columns])
+            ax = famd.plot_row_coordinates(
+                new_data,
+                ax=None,
+                x_component=0,
+                y_component=1,
+                labels=new_data.index,
+                color_labels=['{}'.format(t) for t in new_data[label]],
+                ellipse_outline=False,
+                ellipse_fill=True,
+                show_points=True
+            )
+            plt.show()
+        else:
+            new_data = data.dropna(axis=0)
+            pca = PCA(n_components=2)
+            X_pca = pca.fit_transform(new_data[columns])
+            sns.scatterplot(x=X_pca[:, 0], y=X_pca[:, 1], hue=label, data=new_data)
+            plt.show()
 
     # correlation plot for all features
-    sns.heatmap(data[[label]+columns[:n_feats]].corr())
+    sns.heatmap(data[[label] + columns[:n_feats]].corr())
     plt.show()
 
     # outlier detection just for numeric features
@@ -68,9 +78,9 @@ def explore_global_plot(data, label='label', n_feats=50, id=None, task='classifi
     plt.show()
 
     miss_data = data[columns[:n_feats]].isnull().sum(axis=1)
-    miss_data=miss_data.to_frame()
-    miss_data.columns=['number_of_missing_attributes']
-    miss_data.sort_values('number_of_missing_attributes',inplace=True)
+    miss_data = miss_data.to_frame()
+    miss_data.columns = ['number_of_missing_attributes']
+    miss_data.sort_values('number_of_missing_attributes', inplace=True)
     miss_data['index'] = list(range(0, miss_data.shape[0]))
     sns.jointplot(x="index", y="number_of_missing_attributes", data=miss_data)
     plt.show()
@@ -90,4 +100,3 @@ def explore_local_plot(data, column):
     # box plot for outlier check
     sns.boxplot(data=data[column])
     plt.show()
-
--- preprocess/preprocess_missvalue.py
+++ preprocess/preprocess_missvalue.py
@@ -1,4 +1,6 @@
+from sklearn.experimental import enable_iterative_imputer
 from sklearn import impute
+import fancyimpute as fyi
 import preprocess as pre
 from utils.util import back_args_str
 
@@ -7,12 +9,23 @@ class PreprocessMissvalue(object):
     def __init__(self, method='SimpleImputer', *args, **kwargs):
         '''
         :param method:
-        SimpleImputer
+        1.SimpleImputer
         parameters for SimpleImputer:
             - strategy: mean, median, constant, most_frequent
             - fill_value: When strategy == "constant", fill_value is used to replace all
         occurrences of missing_values.
             - add_indicator: add binary indicators for missing values
+
+        2.IterativeImputer
+        parameters for IterativeImputer:
+            - max_iter: Maximum number of imputation rounds to perform before returning the imputations computed during the final round.
+            - add_indicator: add binary indicators for missing values
+
+        3.KNN
+        parameters for KNN:
+            - k : Number of neighboring rows to use for imputation.
+
+        4.NuclearNormMinimization
         '''
         self.extra = False
         try:
@@ -20,6 +33,10 @@ class PreprocessMissvalue(object):
         except Exception as e:
             self.extra = True
         if self.extra:
+            try:
+                self.method = eval(f"fyi.{method}({back_args_str(*args, **kwargs)})")
+            except Exception as e:
+                raise AttributeError('传入的方法名或者参数不对，无法实例化对象')
             try:
                 self.method = eval(f"pre.{method}({back_args_str(*args, **kwargs)})")
             except Exception as e:
