c042f8b72df0bc1bc4eddba4be15bb2b3a730f0c
bifeng <hubifengmail@163.com>
2019-12-06 11:16:07 +0800

    update explore

--- preprocess/sfsfsf.py
+++ preprocess/sfsfsf.py
@@ -1,7 +1,11 @@
+'''
+
+'''
+import numpy as np
 from sklearn.experimental import enable_iterative_imputer
 from sklearn import impute
 import fancyimpute as fyi
-import preprocess as pre
+import preprocess_expand as exp
 from utils.util import back_args_str
 
 
@@ -38,7 +42,7 @@ class PreprocessMissvalue(object):
             except Exception as e:
                 raise AttributeError('传入的方法名或者参数不对，无法实例化对象')
             try:
-                self.method = eval(f"pre.{method}({back_args_str(*args, **kwargs)})")
+                self.method = eval(f"exp.{method}({back_args_str(*args, **kwargs)})")
             except Exception as e:
                 raise AttributeError('传入的方法名或者参数不对，无法实例化对象')
 
@@ -47,3 +51,41 @@ class PreprocessMissvalue(object):
 
     def transform(self, *args,**kwargs):
         return self.method.transform(*args,**kwargs)
+
+
+class PreprocessOutlier(object):
+    def __init__(self, strategy="filter", fill_value=np.nan, thresh=3.5):
+        '''
+        :param strategy: "filter", "replace"
+        :param fill_value: when startegy="replace", replace outlier with np.nan
+        '''
+        self.strategy = strategy
+        self.fill_value = fill_value
+        self.thresh = thresh
+
+    def fit(self, X, y=None):
+        median = np.median(X, axis=0)
+        diff = np.abs(X - median)
+        med_abs_deviation = np.median(diff, axis=0)
+
+        self.median = median
+        self.med_abs_deviation = med_abs_deviation
+
+        return self
+
+    def transform(self, X):
+        diff = np.abs(X - self.median)
+        modified_z_score = 0.6745 * diff / self.med_abs_deviation
+
+        mask = modified_z_score > self.thresh
+
+        if self.strategy == 'replace':
+            X = X.astype("float")
+            X[mask] = self.fill_value
+        else:
+            mask_row = np.sum(1 - mask, axis=1) == X.shape[1]
+            X = X[mask_row]
+
+        return X
+
+
